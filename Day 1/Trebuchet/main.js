dad7494da9fa292b082374704b5a6f8db376ac61
Aluxies <alexis.lucas.arnaud@gmail.com>
2023-12-01 20:48:09 +0100

    edit : Day 1 : main.js splits data into lines

--- Day 1/Trebuchet/main.js
+++ Day 1/Trebuchet/main.js
@@ -5,11 +5,17 @@ let data;
 try {
 
     data = fs.readFileSync( 'puzzle-inputs.txt', 'utf8' );
-
-    console.log(data);
     
 } catch( e ) {
 
     console.log( 'Error:', e.stack );
+
+};
+
+const lines = data.split('\n');
+
+for ( const line of lines ) {
+
+    console.log( line );
 
 };
